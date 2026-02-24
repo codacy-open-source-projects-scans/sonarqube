13560fe1d95dccb84a308d1fc6f6b91f566f77a7
Codacy Archive <codacy-archive@local>
2026-02-24 01:14:42 +0000

    snapshot 2026-02-24T01:14:42.046208+00:00

--- server/sonar-db-dao/src/main/java/org/sonar/db/rule/RuleDefaultDescriptionContentDto.java
+++ server/sonar-db-dao/src/main/java/org/sonar/db/rule/RuleDefaultDescriptionContentDto.java
@@ -17,12 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonar.db;
+package org.sonar.db.rule;
 
-public interface Pagineable {
-
-  int getOffset();
-
-  int getPageSize();
-
+public record RuleDefaultDescriptionContentDto(String ruleUuid, String content) {
 }
